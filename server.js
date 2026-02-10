be17d16571cf9348cd323a17bcbb51cf0ff0f054
discount-backend <priyadarshinib416@gmail.com>
2026-02-10 20:12:08 +0530

    i commit

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ connectDB();
 const app = express();
 app.use(express.json()); 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/discount-rules", discountRuleRoutes);
